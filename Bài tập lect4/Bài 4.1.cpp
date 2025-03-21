32c2cf961339b829d9403a4fb8fb1dfbd984da1f
23021804 <23021804@vnu.edu.vn>
2025-03-21 23:51:56 +0700

    Add files via upload

---
+++ Bài tập lect4/Bài 4.1.cpp
@@ -0,0 +1,7 @@
+// Hai tính chất quan trọng của một hàm băm là:
+
+// Tính nhất quán (Determinism): Với cùng một đầu vào, hàm băm luôn cho ra cùng một giá trị đầu ra.
+// Điều này rất cần thiết để đảm bảo tính tin cậy trong việc so sánh và lưu trữ dữ liệu.
+
+// Khả năng chống va chạm (Collision Resistance): Rất khó (hoặc gần như không thể) tìm được hai đầu vào khác nhau có cùng giá trị băm. 
+// Điều này giúp bảo vệ dữ liệu khỏi các sự cố không mong muốn và tăng cường tính bảo mật của hệ thống.
